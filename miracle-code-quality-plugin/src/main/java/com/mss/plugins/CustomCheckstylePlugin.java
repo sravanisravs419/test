545e37a3ee71c6873a88b4f003884cefc371f268
sravanisravs419 <sravanisravs4b9@gmail.com>
2024-07-09 11:37:28 +0530

    init

--- miracle-code-quality-plugin/src/main/java/com/mss/plugins/CustomCheckstylePlugin.java
+++ miracle-code-quality-plugin/src/main/java/com/mss/plugins/CustomCheckstylePlugin.java
@@ -5,15 +5,12 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
 
 @Mojo(name = "custom-checkstyle", defaultPhase = LifecyclePhase.VERIFY, requiresDependencyResolution = ResolutionScope.TEST)
 public class CustomCheckstylePlugin extends AbstractMojo {
 
-    @Parameter(defaultValue = "${project}", readonly = true, required = true)
-    private MavenProject project;
+
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
